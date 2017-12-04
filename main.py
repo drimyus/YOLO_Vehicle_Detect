54bbbdc5dd5cc2e2d5bd31360445c74196fcd403
be <dreamyouth@engineer.com>
2017-12-05 05:30:30 +0800

    add the result image

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from yolo_tensor.yolo_small import *
 
 if __name__ == '__main__':
 
-    input_type = 'image'  # 'video'  # 'video' # 'image'
-    input_name = './data/images/test4.jpg'  # 'test_images/test1.jpg' # 'project_video.mp4'
+    input_type = 'video'  # 'video'  # 'video' # 'image'
+    input_name = './data/videos/test.MOV'  # 'test_images/test1.jpg' # 'project_video.mp4'
 
     yolo = yolo_tf()
 
@@ -20,6 +20,7 @@ if __name__ == '__main__':
         cap = cv2.VideoCapture(input_name)
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         out = cv2.VideoWriter('output_small.avi', fourcc, 25.0, (480, 360))  # 480 x 360
+        cnt = 0
         while cap.isOpened():
             ret, frame = cap.read()
 
@@ -29,14 +30,14 @@ if __name__ == '__main__':
 
                 yolo_result = show_results(frame, yolo)
                 cv2.imshow('result', yolo_result)
-                cv2.imwrite('result.jpg', yolo_result)
-                break
 
                 out.write(yolo_result)
 
                 # cv2.waitKey(0)
+                cnt += 1
                 if cv2.waitKey(1) & 0xFF == ord('q'):
-                    break
+                    cv2.imwrite("result{}.jpg".format(cnt), yolo_result)
+
             else:
                 break
         cap.release()
